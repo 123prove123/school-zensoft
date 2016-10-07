3ba7a006e13288c05ae99b2061a14b4235776d6d
Михаил Паутов < >
2016-10-07 21:33:36 +0600

    delete distr files

--- distr/builder/data/group-data.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    let groupData = {
-        groupA1: { level: 1, groupNumber: 'A' },
-        groupA2: { level: 2, groupNumber: 'A' }
-    };
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.default = groupData;
-});
