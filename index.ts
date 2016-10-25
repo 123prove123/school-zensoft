f711ef1bd57949193c0e6f46ed4c68ac43cc0cc9
Olga <davlenbaeva.olga@gmail.com>
2016-10-25 17:55:00 +0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,11 @@
+import Builder from './builder/builder';
+import BuilderHTML from './builder/builder-html';
+import BuilderEvents from './builder/builder-events';
+
+
+// working BuilderHtml, end starter build builder Evenets
+
+
+Builder.run();
+BuilderHTML.run();
+BuilderEvents.run();
